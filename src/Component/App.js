2b19156051f76567a7dd7bb25bfeecacdddfc639
Rushi <rushikesh.21910052@viit.ac.in>
2024-04-11 18:13:18 +0530

    Start coding to create a Netflix clone.

---
+++ src/Component/App.js
@@ -0,0 +1,12 @@
+
+import '../Style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <h1>Rushi</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './Style/index.css';
+import App from './Component/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
